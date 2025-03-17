e2bd195c078fc770d8efc72606aaa442bcd73886
Attila Batky <attila.batky@bt.com>
2025-03-17 16:01:50 +0100

    fix: update jest config to handle css imports

--- jest.config.mjs
+++ jest.config.mjs
@@ -9,6 +9,7 @@ export default {
     '**/*.test.ts', // Only include .test.ts(x) files, spec files are reserved for playwright e2e tests
     '**/*.test.tsx',
   ],
+  moduleNameMapper: { '\\.css$': 'identity-obj-proxy' },
   moduleFileExtensions: ['js', 'jsx', 'ts', 'tsx'],
   setupFilesAfterEnv: ['<rootDir>/setupTests.ts'],
 };

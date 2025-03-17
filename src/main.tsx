370497ed9c85d7933d98ae63def162936c6d67dc
Attila Batky <attila.batky@bt.com>
2025-03-17 12:16:27 +0100

    chore: remove react query devtools from the app

--- src/main.tsx
+++ src/main.tsx
@@ -2,7 +2,6 @@ import * as React from 'react';
 import { ChakraProvider, ColorModeScript } from '@chakra-ui/react';
 import * as ReactDOM from 'react-dom/client';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
-import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
 import theme from './theme';
 import { RouterProvider } from 'react-router-dom';
 import router from './routing/router';
@@ -17,7 +16,6 @@ ReactDOM.createRoot(rootElement).render(
         <ColorModeScript initialColorMode={theme.config.initialColorMode} />
         <RouterProvider router={router}></RouterProvider>
       </ChakraProvider>
-      <ReactQueryDevtools />
     </QueryClientProvider>
   </React.StrictMode>
 );
